e49d83923726edd63e321cf1651566f8545be108
AriZaravelis <47796939+AriZaravelis@users.noreply.github.com>
2023-03-07 13:42:25 -0500

    Delete f1ea927cc6abc807f653d4d8d9a3db8e11a8e805/Notes/CMPT 439 Numerical Computation/MATLAB CMPT 439 Numerical Computation directory

--- f1ea927cc6abc807f653d4d8d9a3db8e11a8e805/Notes/CMPT 439 Numerical Computation/MATLAB CMPT 439 Numerical Computation/HW5/Q2.m
+++
@@ -1,8 +0,0 @@
-% Question 2 Part a
-augMatrix1 = [3 1 -4 7;-2 3 1 -5;2 0 5 10];
-[solution1] = cram(augMatrix1)
-
-
-% Question 2 Part b
-augMatrix2 = [1 -2 4 6;8 -3 2 2;-1 10 2 4];
-[solution2] = cram(augMatrix2)
